9bdba62669ec5fb357dc29f49912a0666f03b62f
AASHISH0709 <aashishshahi0709@gmail.com>
2021-09-14 23:34:44 +0530

    host

--- Donation/settings.py
+++ Donation/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b9%8955zb33s-j*anaeo_1-i#vj&ewkwt%w0)y6g^)0hi3s)ir'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
